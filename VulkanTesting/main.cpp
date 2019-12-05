7c439aa7ba9d0c507a9e2251e346d65c326abad0
Gerard Vivancos <gerard.vivancos@gmail.com>
2019-12-05 18:09:36 +0100

    Initial Commit

---
+++ VulkanTesting/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  VulkanTesting
+//
+//  Created by garbage on 05/12/2019.
+//  Copyright © 2019 garbage. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
