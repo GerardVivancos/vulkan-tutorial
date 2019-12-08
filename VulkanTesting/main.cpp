35b8bf35e0d0e222625f39db03c461a7df2363cb
Gerard Vivancos <gerard.vivancos@gmail.com>
2019-12-08 21:57:25 +0100

    Add some checking methods for extension loading

--- VulkanTesting/main.cpp
+++ VulkanTesting/main.cpp
@@ -4,6 +4,7 @@
 #include <stdexcept>
 #include <functional>
 #include <cstdlib>
+#include <vector>
 
 const int WIDTH = 800;
 const int HEIGHT = 600;
@@ -23,6 +24,9 @@ class HelloTriangleApplication {
     VkInstance instance; // The instance connects the app and the Vulkan library
     
     void initVulkan() {
+        printVulkanSupportedExtensions();
+        printGlfwRequiredExtensions();
+        checkGlfwRequiredExtensionsAvailable();
         createInstance();
     }
     
@@ -33,6 +37,7 @@ class HelloTriangleApplication {
     }
     
     void cleanup() {
+        vkDestroyInstance(instance, nullptr);
         glfwDestroyWindow(window);
         glfwTerminate();
     }
@@ -59,14 +64,14 @@ class HelloTriangleApplication {
         createInfo.pApplicationInfo = &appInfo; // points to the VkApplicationInfo struct above
         /* Now, in order to deal with GLFW windowing we need extensions because Vulkan is platform agnostic.
          GLFW provides a method that returns the list of Vulkan extensions it requires and receives a pointer to an uint which it will use to write the number of these extensions.
+         We wrap this method so we can use it around.
          We then pass the values to the VkInstanceCreateInfo struct.
          */
+
         uint32_t glfwExtensionCount = 0;
-        const char** glfwExtensions;
-        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+        const char** glfwExtensions = listGlfwRequiredExtensions(&glfwExtensionCount);
         createInfo.enabledExtensionCount = glfwExtensionCount;
         createInfo.ppEnabledExtensionNames = glfwExtensions;
-        
         createInfo.enabledLayerCount = 0; // Won't use validation layers now
         
         /* Actually create the instance
@@ -80,6 +85,73 @@ class HelloTriangleApplication {
         if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
             throw std::runtime_error("Failed to create instance");
         }
+    }
+
+    const std::vector<VkExtensionProperties> listVulkanSupportedExtensions(uint32_t* count = nullptr) {
+        uint32_t extensionCount = 0;
+
+        // First call is so we can know the count and initialize a vector this big
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+        std::vector<VkExtensionProperties> extensions(extensionCount);
+
+        // Actually retrieve the extension list
+        // First argument is for filtering, not doing that now.
+        // vector::data is a pointer to the underlying data array (http://www.cplusplus.com/reference/vector/vector/data/)
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+        if (count != nullptr) {
+            *count = extensionCount;
+        }
+        return extensions;
+    }
+
+    void printVulkanSupportedExtensions() {
+        uint32_t extensionCount = 0;
+        std::vector<VkExtensionProperties> extensions = listVulkanSupportedExtensions(&extensionCount);
+        std::cout << "Available extensions: " << extensionCount << std::endl;
+        // "const auto&" lets us access the values without a copy (&), and prevents their accidental modification (const)
+        for (const auto& extension: extensions) {
+            std::cout << "--\t" << extension.extensionName << std::endl;
+        }
+    }
+
+    const char** listGlfwRequiredExtensions(uint32_t* count = nullptr) {
+        // GLFW provides a method that returns the list of Vulkan extensions it requires and receives a pointer to an uint which it will use to write the number of these extensions.
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        if (count != nullptr) {
+            *count = glfwExtensionCount;
+        }
+        return glfwExtensions;
+    }
+
+    void printGlfwRequiredExtensions() {
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions = listGlfwRequiredExtensions(&glfwExtensionCount);
+        std::cout << "Required extensions for GLFW: " << glfwExtensionCount << std::endl;
+        for (int i = 0; i < glfwExtensionCount; i++)  {
+            std::cout << "**\t" << glfwExtensions[i] << std::endl;
+        }
+    }
+
+    bool checkGlfwRequiredExtensionsAvailable() {
+        uint32_t glfwExtensionCount = 0;
+        uint32_t vulkanExtensionCount = 0;
+        uint32_t matchingExtensions = 0;
+        const char** glfwExtensions = listGlfwRequiredExtensions(&glfwExtensionCount);
+        const std::vector<VkExtensionProperties> vulkanExtensions = listVulkanSupportedExtensions(&vulkanExtensionCount);
+
+        for (int i = 0; i < glfwExtensionCount; i++) {
+            for (const auto& vulkanExtension: vulkanExtensions) {
+                if (0 == strcmp(glfwExtensions[i], vulkanExtension.extensionName)) {
+                matchingExtensions++;
+                }
+            }
+        }
+
+        // Do all the required extensions match?
+        return matchingExtensions == glfwExtensionCount;
     }
 };
 
